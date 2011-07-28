ab36f6f3bea224b8e1c2d97d936938dc315b7a19
Tom <tom@imakethin.gs>
2011-07-28 19:14:23 +0100

    class!

--- src/SheepTest.java
+++ src/SheepTest.java
@@ -9,8 +9,11 @@ public class SheepTest extends PApplet {
 
 
 
-	OpenCV opencv;
+	public OpenCV opencv;
 	ControlP5 controlP5;
+	PImage frame;
+	
+	SheepIdentifier sheepFinder;
 
 	int thresh = 0;   //threshold value 
 	int minBlobSize = 10;
@@ -35,15 +38,16 @@ public class SheepTest extends PApplet {
 
 
 	public void setup() {
-	  size (1024,600);
+	  size (1280,800);
 	  colorMode(RGB, 100);
 	  opencv = new OpenCV( this );
 	  opencv.allocate(640,480);
 	 
 	  opencv.capture(640,480);
-
+	  
+	  sheepFinder = new SheepIdentifier(this);
 	  //green removal buffer. Allocated once to save time
-	  removeGreenBuffer = createImage(640,480, RGB);
+	  
 
 	  controlP5 = new ControlP5(this);
 	  controlP5.addSlider("thresh",0,255,0,650,260,100,14).setId(4);
@@ -80,71 +84,20 @@ public class SheepTest extends PApplet {
 
 	public void draw() {
 	  background(0,0,0);
+	  opencv.read();
+	  frame = opencv.image();
+	  sheepFinder.minBlobSize = minBlobSize;
+	  sheepFinder.maxBlobSize = maxBlobSize;
+	  sheepFinder.greenThreshLow = greenThreshLow;
+	  sheepFinder.greenThreshHigh = greenThreshHigh;
+	  sheepFinder.colourSampleArea = colourSampleArea;  //radius of area to colour sample around blob centroids
+	  sheepFinder.sheepSaturationDetection = sheepSaturationDetection;
+	  
+	  image(frame,640,0,160,120);
+	  sheepFinder.update(frame);
+	  
+	  image(sheepFinder.removeGreenBuffer, 0,0);
 
-
-	  opencv.read();           // grab frame from camera
-	  //opencv.copy(colorBuffer);
-	  //remember the colour image
-	  colorBuffer = opencv.image();
-	  image(colorBuffer,640,0,160,120);
-
-
-	  //opencv.invert();
-	  // opencv.threshold(150, 0, OpenCV.THRESH_OTSU | OpenCV.THRESH_TOZERO);    // set black & white threshold 
-	  //opencv.absDiff();                           //  Creates a difference image
-	  //opencv.convert(OpenCV.GRAY);                //  Converts to greyscale
-	  //opencv.blur(OpenCV.BLUR, 6);                //  Blur to remove camera noise
-
-	  //opencv.threshold(thresh);
-	  removeGreen(opencv.image());
-	  image(removeGreenBuffer, 0,0);
-	  opencv.copy(removeGreenBuffer);
-
-	  // find blobs
-	  Blob[] blobs = opencv.blobs( minBlobSize, maxBlobSize, 100, false, OpenCV.MAX_VERTICES*4 );
-	  removeGreenBuffer.loadPixels();
-
-
-	  for( int i=0; i < blobs.length; i++) {
-	    //for each blob work out the average colour under it
-
-	    int pixelCount = 0 ;  //count of how many pixels we examine
-	    int avgRed = 0;
-	    int avgGreen = 0;
-	    int avgBlue = 0;
-
-	    Blob test = blobs[i];
-	    if(test.area > minBlobSize || test.area > maxBlobSize) {
-	      for(int px = -colourSampleArea; px < colourSampleArea; px++) {
-	        for(int py = -colourSampleArea; py < colourSampleArea; py++) {      
-	          if(test.centroid.x + px < 640 && test.centroid.x + px > 0 && test.centroid.y + py < 480 && test.centroid.y + py > 0) {
-	            pixelCount ++;
-
-	            avgRed +=   ( colorBuffer.pixels[(test.centroid.x + px)  + (test.centroid.y + py) * 640    ] >> 16) & 0xFF;
-	            avgGreen += ( colorBuffer.pixels[(test.centroid.x + px)  + (test.centroid.y + py) * 640    ] >> 8) & 0xFF;
-	            avgBlue +=  ( colorBuffer.pixels[(test.centroid.x + px)  + (test.centroid.y + py) * 640    ] ) & 0xFF;
-	          }
-	        }
-	      }
-	      avgRed /= pixelCount;
-	      avgGreen /= pixelCount;
-	      avgBlue /= pixelCount;
-	      int avgColor =  color (avgRed,avgGreen,avgBlue);
-
-
-
-	      if(saturation(avgColor) < sheepSaturationDetection) {      //this blob is a sheep as its colour saturation is low (close to white)
-	        textFont(myFont);
-	        fill(255,0,0);
-	        text("Sheep",  blobs[i].centroid.x, blobs[i].centroid.y + 20);
-	      }
-
-	      fill(avgColor);
-
-	      ellipse(blobs[i].centroid.x, blobs[i].centroid.y,20,20);   //draw the blob with the average colour underneath it
-	      fill(0,0,0);
-	    }
-	  }
 	  colorMode(HSB, 360,100,100);
 	  fill(greenThreshLow,100,100);
 	  rect(630,380,20,20);
@@ -155,30 +108,8 @@ public class SheepTest extends PApplet {
 	  
 	}
 
-	public void removeGreen(PImage in) {
-	  in.loadPixels();
-	  removeGreenBuffer.loadPixels();
-	  int c;
-	  for(int i = 0; i < 640 * 480; i++) {
-	    c =  color((in.pixels[i] >> 16) & 0xFF, (in.pixels[i] >> 8) & 0xFF, (in.pixels[i] ) & 0xFF );
+	
 
-	    if(hue(c) >= greenThreshLow && hue(c) <= greenThreshHigh) {
-	      removeGreenBuffer.pixels[i] = 0;
-	    } 
-	    else {     
-	      removeGreenBuffer.pixels[i] = 16777215;
-	    }
-	  }
-	  removeGreenBuffer.updatePixels();
-
-	  // return removeGreenBuffer;
-	}
-
-
-	//sweep around the centroid looking for a region of colour 
-	public boolean isThisACar(Blob blob) {
-	  return false;
-	}
 
 	public void keyPressed() {
 	}
